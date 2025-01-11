519f5e78ede6db8865a2b81994930a17c9122096
ImFenyx <103691581+ImFenyx@users.noreply.github.com>
2025-01-11 11:32:25 -0300

    feat: :memo: Aulas 1-8, Exercício da aula 8 sendo feito

---
+++ Aulas/aula 7/index.js
@@ -0,0 +1,5 @@
+console.log("Iago"); // Pode usar aspas duplas
+console.log('Iago'); // Pode usar aspas simples
+console.log(`Iago`); // Pode usar crase
+
+console.log(123456, 15.85, 'Iago de novo'); // Pode colocar números, inclusive vários em uma linha e tipos diferentes
