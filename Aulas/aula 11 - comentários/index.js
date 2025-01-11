690023739844db2c11a8712837ca2a4bab2bb822
ImFenyx <103691581+ImFenyx@users.noreply.github.com>
2025-01-11 17:27:42 -0300

    feat: :memo: Melhorado um pouco as estruturas dos arquivos, exercício 8 feito, aulas 9-13

---
+++ Aulas/aula 11 - comentários/index.js
@@ -0,0 +1,11 @@
+// Comentário
+console.log("Olá mundo"); // Aqui tem outro comentário
+
+
+/**
+ * Um bloco de comentário
+ * Multilinhas
+ * -----------------------
+ * Título
+ * Lorem ipsum
+ */
---
+++ Aulas/aula 13 - let vars/index.js
@@ -0,0 +1,24 @@
+let nome = 'João'; //* String
+
+console.log(nome, `nasceu em 1984.`);
+console.log(`Em 2000`, nome, `conheceu Maria.`);
+console.log(nome, `casou-se com Maria em 2012.`)
+console.log(`Maria teve 1 filho com`, nome, `em 2015.`)
+console.log(`O filho de`, nome, `se chama Eduardo.`)
+
+// let nome; //* undefined
+// console.log(nome); //* a variável terá um "valor" que indica ausência de valor chamado undefined
+// nome = "Teste"; //* Inicializando a variável
+
+//* Não podemos criar variáveis com palavra reservadas
+// let if -> //* não funciona
+
+/**
+ //* Variáveis precisam ter nomes significativos (objetivos, claros e coerentes)
+ //* Não pode começar o nome de uma variável com um número
+ //* Não pode conter espaços ou traços
+ //* Utilizamos camelCase
+ //* Case-sensitive
+ //* Não podemos redeclarar variáveis com let
+ //* NÃO UTILIZE VAR, UTILIZE LET
+*/
