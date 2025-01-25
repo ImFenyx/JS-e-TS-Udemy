02379e6f67ea9c25904767b9f14f610565746ce9
ImFenyx <103691581+ImFenyx@users.noreply.github.com>
2025-01-25 12:42:46 -0300

    feat: :memo: aula 27 - objeto math

---
+++ Aulas/aula 27 - Objeto Math/index.js
@@ -0,0 +1,15 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1); // 9 //* arredonda para baixo, sempre arredonda para o número inteiro mais próximo, ignorando as casas decimais
+// let num2 = Math.ceil(num1); // 10 //* arredonda para cima, sempre arredonda para o número inteiro mais próximo, de acordo com as casas decimais
+// let num2 = Math.round(num1); // 10 //* arredonda para o número inteiro mais próximo, se as casas decimais forem maiores que 0.5, arredonda para cima, se forem menores que 0.5, arredonda para baixo
+// let num2 = Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6); // 1500 //* retorna o maior número
+// let num2 = Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6); // -50 //* retorna o maior número
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); //* número aleatório entre 5 e 10, 10 não incluso com arredondamento.
+//console.log(Math.random()); //* número aleatório entre 0 e 1, 1 não incluso.
+//console.log(aleatorio); //* const aleatorio
+console.log(num2);
+//console.log(Math.PI); //* número pi
+//console.log(Math.pow(2, 10)); //* 2 elevado a 10 (potenciação - 2^10)
+
+console.log(100 / 0); //Infinity //* divisão por 0
+// Cuidado com a divisão por 0, pois pode gerar problemas no código. Use try catch para tratar erros.
